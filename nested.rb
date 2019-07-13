a784f7afb2e56bf4d1e6f3d6b700c5279dab6f00
rabab raza ali <robin.ali85@yahoo.com>
2019-07-13 16:22:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash =[:known_for]
+	programmer_hash=
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
 end
 
 def dennis_ritchies_language
